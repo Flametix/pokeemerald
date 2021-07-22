9f8bc0dadf4ab18ff4c5b1be89bee4770e53ea6d
Flametix <44883711+Flametix@users.noreply.github.com>
2021-07-22 14:05:11 -0400

    Fix Intrepid Sword/death switchin animations applying to wrong mon
    Just use BS_SCRIPTING I guess. I could have fixed Illusioner this way but it'd require a separate script I think. I don't know how the battle engine does this

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -4633,7 +4633,7 @@ BattleScript_AttackerFormChangeEnd3::
 BattleScript_BattlerAbilityStatRaiseOnSwitchIn::
 	statbuffchange MOVE_EFFECT_AFFECTS_USER | STAT_BUFF_NOT_PROTECT_AFFECTED | MOVE_EFFECT_CERTAIN, NULL
 	setgraphicalstatchangevalues
-	playanimation BS_ATTACKER, B_ANIM_STATS_CHANGE, sB_ANIM_ARG1
+	playanimation BS_SCRIPTING, B_ANIM_STATS_CHANGE, sB_ANIM_ARG1
 	waitanimation
 	printstring STRINGID_BATTLERABILITYRAISEDSTAT
 	waitmessage B_WAIT_TIME_LONG
