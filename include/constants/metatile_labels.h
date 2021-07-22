dcba859227cc120220d6cc95ded575ee91e37362
Flametix <44883711+Flametix@users.noreply.github.com>
2021-07-22 01:23:20 -0400

    More demo work

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -135,6 +135,12 @@
 #define METATILE_Contest_FloorShadow         0x261
 #define METATILE_Contest_WallShadow          0x221
 
+// gTileset_DemoChristmasDucks
+#define METATILE_DemoChristmasDucks_BLACK_DOOR      0x21B
+#define METATILE_DemoChristmasDucks_BLACK_DOOR_TOP  0x213
+#define METATILE_DemoChristmasDucks_WINDOW1         0x210
+#define METATILE_DemoChristmasDucks_WINDOW2         0x218
+
 // gTileset_Dewford
 #define METATILE_Dewford_Door              0x225
 #define METATILE_Dewford_Door_BattleTower  0x25D
