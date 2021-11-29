c3e749355d6f57df4201d929d754a3ad08d29294
Flametix <44883711+Flametix@users.noreply.github.com>
2021-11-28 21:23:31 -0500

    remove duplicate sendmontopc

--- include/pokemon.h
+++ include/pokemon.h
@@ -449,7 +449,5 @@ void DestroyMonSpritesGfxManager(u8 managerId);
 u8 *MonSpritesGfxManager_GetSpritePtr(u8 managerId, u8 spriteNum);
 
 void CreateShinyMonWithNature(struct Pokemon *mon, u16 species, u8 level, u8 nature);
-u8 SendMonToPC(struct Pokemon* mon);
-
 
 #endif // GUARD_POKEMON_H
