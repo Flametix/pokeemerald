38e2fe6369a860b176c897f7a5685dfb3f8cfca7
Flametix <44883711+Flametix@users.noreply.github.com>
2022-06-07 17:36:14 -0400

    Update mention of gTrainerId to sTrainerId

--- src/main_menu.c
+++ src/main_menu.c
@@ -1264,7 +1264,7 @@ static void HighlightSelectedMainMenuItem(u8 menuType, u8 selectedMenuItem, s16 
 #define tMaySpriteId data[11]
 
 // DEBUG from TPP Trick or Treat house
-extern u16 gTrainerId;
+extern u16 sTrainerId;
 static void Task_NewGameSkipSpeech(u8 taskId)
 {
     SetGpuReg(REG_OFFSET_DISPCNT, 0);
@@ -1281,7 +1281,7 @@ static void Task_NewGameSkipSpeech(u8 taskId)
         u16 rng = Random();
         gSaveBlock2Ptr->playerGender = (rng & 0xFF) % 1;
         NewGameBirchSpeech_SetDefaultPlayerName((rng >> 8) % 20);
-        gTrainerId = Random();
+        sTrainerId = Random();
     }
 
     FadeOutBGM(4);
