2c444b429f07fcecea3e3da327a124b17b093edc
Flametix <44883711+Flametix@users.noreply.github.com>
2022-06-12 23:09:06 -0400

    Update Galarian Farfetch'd and wild encounters
    credit to expansion

--- src/data/graphics/pokemon.h
+++ src/data/graphics/pokemon.h
@@ -3117,11 +3117,11 @@ const u32 gMonStillFrontPic_Swanna[] = INCBIN_U32("graphics/pokemon/duck/front.4
 const u8 gMonIcon_Swanna[] = INCBIN_U8("graphics/pokemon/duck/icon.4bpp");
 const u8 gMonFootprint_Swanna[] = INCBIN_U8("graphics/pokemon/duck/footprint.1bpp");
 
-const u32 gMonPalette_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd/normal.gbapal.lz");
-const u32 gMonBackPic_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd/back.4bpp.lz");
-const u32 gMonShinyPalette_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd/shiny.gbapal.lz");
-const u32 gMonStillFrontPic_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd/front.4bpp.lz");
-const u8 gMonIcon_FarfetchdGalar[] = INCBIN_U8("graphics/pokemon/farfetchd/icon.4bpp");
+const u32 gMonPalette_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd_galar/normal.gbapal.lz");
+const u32 gMonBackPic_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd_galar/back.4bpp.lz");
+const u32 gMonShinyPalette_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd_galar/shiny.gbapal.lz");
+const u32 gMonStillFrontPic_FarfetchdGalar[] = INCBIN_U32("graphics/pokemon/farfetchd_galar/front.4bpp.lz");
+const u8 gMonIcon_FarfetchdGalar[] = INCBIN_U8("graphics/pokemon/farfetchd_galar/icon.4bpp");
 const u8 gMonFootprint_FarfetchdGalar[] = INCBIN_U8("graphics/pokemon/farfetchd/footprint.1bpp");
 
 const u32 gMonPalette_Sirfetchd[] = INCBIN_U32("graphics/pokemon/sirfetchd/normal.gbapal.lz");
